84d9032f750a1433857a19a3bdd569f1cec84b13
stuti027 <stutishiv18@gmail.com>
2025-04-12 03:49:36 +0530

    signup link

---
+++ app/src/main/java/com/app/remedi_final/LoginPage.java
@@ -0,0 +1,23 @@
+package com.app.remedi_final;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+    }
+    public void signup() {
+        Intent intent = new Intent(this, SignUp.class);
+        startActivity(intent);
+    }
+}
