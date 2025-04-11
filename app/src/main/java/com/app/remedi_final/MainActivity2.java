7e2d48616874f1a648c7efcc12d5a6195fed2a0a
stuti027 <stutishiv18@gmail.com>
2025-04-11 13:26:28 +0530

    reminder page commit

---
+++ app/src/main/java/com/app/remedi_final/MainActivity2.java
@@ -0,0 +1,13 @@
+package com.app.remedi_final;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class MainActivity2 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
