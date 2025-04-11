41423486d8859ecdc4f6fff514128100ac22bcc6
stuti027 <stutishiv18@gmail.com>
2025-04-12 04:39:25 +0530

    layouts commit

--- app/src/main/java/com/app/remedi_final/LoginPage.java
+++ app/src/main/java/com/app/remedi_final/LoginPage.java
@@ -1,7 +1,9 @@
 package com.app.remedi_final;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,15 +11,19 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class LoginPage extends AppCompatActivity {
+public class LoginPage extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
     }
-    public void signup() {
+    public void signup(View view) {
         Intent intent = new Intent(this, SignUp.class);
         startActivity(intent);
+    }
+    public void acctdone(View vie) {
+        Intent intent1 = new Intent(this, HomePage.class);
+        startActivity(intent1);
     }
 }
